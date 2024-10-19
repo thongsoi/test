099589fd22924b4248923f4cea16f2dbd48fe20d
Jack Wattanapong <w.thongsoi@gmail.com>
2024-10-19 19:01:34 +0700

    140

--- database/database.go
+++ database/database.go
@@ -67,36 +67,3 @@ func GetDB() *sql.DB {
 	}
 	return DB
 }
-
-/*
-package database
-
-import (
-	"database/sql"
-	"log"
-
-	_ "github.com/lib/pq" // PostgreSQL driver
-)
-
-var db *sql.DB
-
-// InitDB initializes a global DB connection
-func InitDB() *sql.DB {
-	var err error
-	dsn := "postgres://dev1:dev1pg@localhost:5432/biomassx?sslmode=disable" // your connection string
-	db, err = sql.Open("postgres", dsn)
-	if err != nil {
-		log.Fatal("Error connecting to the database:", err)
-	}
-
-	return db
-}
-
-// GetDB returns the existing DB instance
-func GetDB() *sql.DB {
-	if db == nil {
-		log.Fatal("Database connection is not initialized")
-	}
-	return db
-}
-*/
